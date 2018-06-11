77bca7e58101d66ad3fb39ec8fbda9b0952f038d
elen7c9 <enasiyati@gmail.com>
2018-06-11 18:44:48 +0300

    Revert "1"
    This reverts commit 9a2a40a4280a66b6406015ac9a859894deb0cc0c.

--- app/src/main/java/com/dz/elen/homeworks/Homework2Activity.java
+++ app/src/main/java/com/dz/elen/homeworks/Homework2Activity.java
@@ -1,7 +1,7 @@
 package com.dz.elen.homeworks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Homework2Activity extends AppCompatActivity {
 
